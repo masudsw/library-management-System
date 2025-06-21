6f1e3527dcdee97d4cd3323955460639511d1703
Golam Mustafa <golammustafa@gmail.com>
2025-06-20 23:41:23 -0400

    borrow summary aggregation function added

--- src/modules/borrow/borrow.controller.ts
+++ src/modules/borrow/borrow.controller.ts
@@ -26,8 +26,52 @@ const borrowBook = async (req: Request, res: Response) => {
     }
 }
 
+const borrowedBookSummary = async (req: Request, res: Response) => {
+    try {
+        const data = await Borrow.aggregate([
+            {
+                $group: {
+                    _id: "$book",
+                    totalQuantity: { $sum: "$quantity" }
+                }
 
+            },
+            {
+                $lookup: {
+                    from: "books",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "bookInfo"
+                },
+            },
+            { $unwind: "$bookInfo" },
+            {
+                $project: {
+                    _id: 0,
+                    book: {
+                        title: "$bookInfo.title",
+                        isbn: "$bookInfo.isbn",
+                    },
+                    totalQuantity: 1,
+                },
+            },
+
+        ]);
+        res.send({
+            success: true,
+            data
+        });
+    } catch (error: any) {
+        res.send(
+            {
+                success: false,
+                message: error.message || "Aggregation failed"
+            }
+        );
+    }
+
+};
 export const borrowController = {
     borrowBook,
-    // borrowedBookSummary
+    borrowedBookSummary
 }
